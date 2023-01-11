3e5e3574b893c5bdf82b8267d8e20e0dacdb913b
LuthGom <lucianomendes23@yahoo.com>
2023-01-10 23:55:56 -0300

    Importand clients service no app mdule para roteamento

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsController } from './clients.controller';
+import { ClientService } from './clients.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [ClientsController],
+  providers: [ClientService],
 })
 export class AppModule {}
