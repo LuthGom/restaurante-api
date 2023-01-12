5e3fcd790ce8add420ccecbef345e7c7dda4805e
LuthGom <lucianomendes23@yahoo.com>
2023-01-12 00:13:17 -0300

    Configurando database provider do MongoDB

---
+++ src/clients/database/database.providers.ts
@@ -0,0 +1,12 @@
+import * as mongoose from 'mongoose';
+
+const MONGO_DB_PASSWORD = process.env.MONGO_DB_PASSWORD;
+export const databaseProviders = [
+  {
+    provide: 'DATABASE_CONNECTION',
+    useFactory: (): Promise<typeof mongoose> =>
+      mongoose.connect(
+        `mongodb+srv://livraria_api_ts:${MONGO_DB_PASSWORD}.lifxpal.mongodb.net/test`,
+      ),
+  },
+];
