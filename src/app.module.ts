93579cb1bec5c316516dccf8b444305508c46a7a
LuthGom <lucianomendes23@yahoo.com>
2023-01-14 17:09:29 -0300

    passando infos databse_user para .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { ClientsController } from './clients/clients.controller';
-import { ClientsService } from './clients/clients.service';
 import { ClientsModule } from './clients/clients.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ClientsModule],
-  controllers: [ClientsController],
-  providers: [ClientsService],
+  imports: [
+    ClientsModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.NESTJS_MONGODB_USER}:${process.env.NESTJS_MONGODB_PASSWORD}@apicluster.lifxpal.mongodb.net/restaurante-api`,
+    ),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
