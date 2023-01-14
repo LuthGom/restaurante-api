c2e6b5f52b6a3b1b77af8047f5dff4454f34815c
LuthGom <lucianomendes23@yahoo.com>
2023-01-13 23:52:00 -0300

    Definindo a model Client

--- src/clients/entities/client.entity.ts
+++ src/clients/entities/client.entity.ts
@@ -1,1 +1,24 @@
-export class Client {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type ClientDocument = HydratedDocument<Client>;
+
+@Schema()
+export class Client {
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: String, required: true })
+  email: string;
+
+  @Prop({ type: String, required: true })
+  username: string;
+
+  @Prop({ type: String, required: true })
+  password: string;
+
+  @Prop([Date])
+  createdAt: Date;
+}
+
+export const ClienteSchema = SchemaFactory.createForClass(Client);
