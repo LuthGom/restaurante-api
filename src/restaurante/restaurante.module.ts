8880d700d116b80dededf807762583d771e7dad3
LuthGom <lucianomendes23@yahoo.com>
2023-01-15 19:26:18 -0300

    importando providers, services e DatabaseModule do restaurante para restaurante.module

--- src/restaurante/restaurante.module.ts
+++ src/restaurante/restaurante.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { RestauranteService } from './restaurante.service';
 import { RestauranteController } from './restaurante.controller';
-
+import { RestauranteService } from './restaurante.service';
+import { restaurantProviders } from './restaurante.providers';
+import { DatabaseModule } from '../shared/database/database.module';
 @Module({
+  imports: [DatabaseModule],
   controllers: [RestauranteController],
-  providers: [RestauranteService]
+  providers: [RestauranteService, ...restaurantProviders],
 })
 export class RestauranteModule {}
