f2bcd00e06016cad9648e236e191d2d90085b782
LuthGom <lucianomendes23@yahoo.com>
2023-01-15 19:25:35 -0300

    ajustando nome do restauranteProviders

--- src/restaurante/restaurante.providers.ts
+++ src/restaurante/restaurante.providers.ts
@@ -1,7 +1,7 @@
 import { Connection } from 'mongoose';
 import { ResturantSchema } from 'src/Schemas/restaurant.schema';
 
-export const clientsProviders = [
+export const restaurantProviders = [
   {
     provide: 'RESTAURANT_MODEL',
     useFactory: (connection: Connection) => connection.model('Restaurant', ResturantSchema),
