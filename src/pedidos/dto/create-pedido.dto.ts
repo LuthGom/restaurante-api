486731b822835f6bee88aee6bdefbecb1348e61e
LuthGom <lucianomendes23@yahoo.com>
2023-01-15 20:07:26 -0300

    criando DTO para pedidos

--- src/pedidos/dto/create-pedido.dto.ts
+++ src/pedidos/dto/create-pedido.dto.ts
@@ -1,1 +1,7 @@
-export class CreatePedidoDto {}
+import { Document } from 'mongoose';
+export class CreatePedidoDto extends Document {
+  readonly itens: string[];
+  readonly total: number;
+  readonly unidade: object;
+  readonly cliente: object;
+}
