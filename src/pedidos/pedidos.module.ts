b48c055f1b8ff96b06b96e2b665ba7d6277634dd
LuthGom <lucianomendes23@yahoo.com>
2023-01-15 20:08:36 -0300

    Importando DatabaseModule no PedidosModule

--- src/pedidos/pedidos.module.ts
+++ src/pedidos/pedidos.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PedidosService } from './pedidos.service';
 import { PedidosController } from './pedidos.controller';
+import { DatabaseModule } from 'src/shared/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [PedidosController],
-  providers: [PedidosService]
+  providers: [PedidosService],
 })
 export class PedidosModule {}
