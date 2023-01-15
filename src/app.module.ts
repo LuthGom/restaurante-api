7a9355f605436d3cfc37574bce30cdf71cfa193d
LuthGom <lucianomendes23@yahoo.com>
2023-01-15 19:32:11 -0300

    criando estrutura para a Schema pedidos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 
 import { ClientsModule } from './clients/clients.module';
 import { RestauranteModule } from './restaurante/restaurante.module';
+import { PedidosModule } from './pedidos/pedidos.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ClientsModule, RestauranteModule],
+  imports: [ConfigModule.forRoot(), ClientsModule, RestauranteModule, PedidosModule],
   controllers: [],
   providers: [],
 })
