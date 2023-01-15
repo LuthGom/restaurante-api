f4f360fc6b5085f9d6ebae73edd02241c90a585f
LuthGom <lucianomendes23@yahoo.com>
2023-01-15 16:38:53 -0300

    passando extends Document do mongoose para o clientDTO

--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -1,7 +1,9 @@
-export class CreateClientDto {
-  name: string;
-  email: string;
-  username: string;
-  password: string;
-  creatAt: Date;
+import { Document } from 'mongoose';
+
+export class CreateClientDto extends Document {
+  readonly name: string;
+  readonly email: string;
+  readonly username: string;
+  readonly password: string;
+  readonly creatAt: Date;
 }
