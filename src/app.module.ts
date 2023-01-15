f681a46f8d4424801380c0ebdb3fc5e1b9c329e9
LuthGom <lucianomendes23@yahoo.com>
2023-01-15 16:41:54 -0300

    retirando a conexão direto do AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { ClientsModule } from './clients/clients.module';
-import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    ClientsModule,
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(
-      `mongodb+srv://${process.env.NESTJS_MONGODB_USER}:${process.env.NESTJS_MONGODB_PASSWORD}@apicluster.lifxpal.mongodb.net/restaurante-api`,
-    ),
-  ],
+  imports: [ConfigModule.forRoot(), ClientsModule],
   controllers: [],
   providers: [],
 })
