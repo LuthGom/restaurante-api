b9796e6389356df464aca3e71ab517f7e1cfc3e3
LuthGom <lucianomendes23@yahoo.com>
2023-01-15 23:33:46 -0300

    passando providers para module

--- src/pedidos/pedidos.module.ts
+++ src/pedidos/pedidos.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PedidosService } from './pedidos.service';
 import { PedidosController } from './pedidos.controller';
 import { DatabaseModule } from 'src/shared/database/database.module';
+import { pedidosProviders } from './pedidos.providers';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [PedidosController],
-  providers: [PedidosService],
+  providers: [PedidosService, ...pedidosProviders],
 })
 export class PedidosModule {}
